ecf0dc9074306d21bf1843c49d1d430e31dc2ddc
therealsqvad <nikita_klepov@mail.ru>
2018-09-24 23:53:29 +0700

    with timer/without speed

--- src/typeracer/typeracer.js
+++ src/typeracer/typeracer.js
@@ -1,36 +1,43 @@
+document.onselectstart = test;
+function test() {return false;}
+var c = 60;
 function new_game(){
-    generate_text();
-    document.getElementById("input_value").disabled = false;
+    setTimeout("document.getElementById('task').textContent='3...'",1000);
+    setTimeout("document.getElementById('task').textContent='2...'",2000);
+    setTimeout("document.getElementById('task').textContent='1...'",3000);
+    setTimeout("generate_text(); document.getElementById('intext').disabled = false; c = 60; timedCount();",4000); 
+    
+}
+
+function timedCount()
+{
+    if(c>=0){
+document.getElementById('timer').textContent=c;
+c--;
+setTimeout("timedCount()",1000);}
+else{
+    if(confirm("Начать заново?")){
+        new_game();
+    }
+}
 }
 
 
 function check(){
-if (document.getElementById("input_value").value == document.getElementById("Out_txt").textContent)
-{document.getElementById("input_value").style.borderColor="green"}
+    console.log(document.getElementById("intext").value);
+    if (document.getElementById("intext").value == document.getElementById("task").textContent)
+{document.getElementById("intext").style.borderColor="green";} 
 else{
-    document.getElementById("input_value").style.borderColor="red";
+    if (document.getElementById("intext").value == document.getElementById("task").textContent.substr(0,document.getElementById("intext").value.length))
+    {document.getElementById("intext").style.borderColor="gray";
 }
-};
-
-function checkin(){
-    console.log(document.getElementById("input_value").value);
-    if (document.getElementById("input_value").value == document.getElementById("Out_txt").textContent.substr(0,document.getElementById("input_value").value.length))
-    {document.getElementById("input_value").style.borderColor="green"}
     else{
-        document.getElementById("input_value").style.borderColor="red";
-    }
+        document.getElementById("intext").style.borderColor="red";
+    }}
 };
-
-
-
-
-document.onselectstart = test;
-function test() {
-    return false
-    }
 
 function generate_text() {
-    var Txts = new Array(15, "Ежедневно делайте зарядку и различные физические упражнения. Развивайте своё тело и дух.",
+    var texts = new Array(15, "Ежедневно делайте зарядку и различные физические упражнения. Развивайте своё тело и дух.",
             "Будьте благодарны Вселенной за каждый прожитый день.",
             "Любите себя таким, какой вы есть, со всеми достоинствами и недостатками",
             "Верьте только в самое лучшее и тогда в вашей жизни станет ещё больше приятных сюрпризов и хорошего настроения",
@@ -45,5 +52,5 @@ function generate_text() {
             "Ежемесячно откладывайте от 10 % от своего дохода. Инвестируйте в своё будущее",
             "Контролируйте свои эмоции. Старайтесь как можно чаще улыбаться и быть в отличном настроении");
     var RandomNumber = Math.round(Math.random() * (15) + 1);
-    document.getElementById("Out_txt").textContent = Txts[RandomNumber];
+    document.getElementById("task").textContent = texts[RandomNumber];
 }
