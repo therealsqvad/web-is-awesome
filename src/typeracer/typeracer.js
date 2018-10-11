4cd8b300d2b39112fc80f63de8d0d2ff81382211
therealsqvad <nikita_klepov@mail.ru>
2018-10-11 20:57:33 +0700

    таймер подсечивается красным и увеличивается

--- src/typeracer/typeracer.js
+++ src/typeracer/typeracer.js
@@ -1,18 +1,22 @@
 document.onselectstart = test;
 function test() {return false;}
-var c = 60;
-var sc =0;
-var speed = 0;
+let c = 12;
+let sc =0;
+let speed = 0;
+let lifes;
+let is_paused = false;
 function new_game(){
+    console.log(getComputedStyle(document.getElementById('timer')).fontSize); 
     setTimeout("document.getElementById('task').textContent='3...'",1000);
     setTimeout("document.getElementById('task').textContent='2...'",2000);
     setTimeout("document.getElementById('task').textContent='1...'",3000);
-    setTimeout("generate_text(); document.getElementById('intext').disabled = false; c = document.getElementById('task').textContent.length; timedCount(); speedTimer();",4000);     
+    setTimeout(start_game,4000);     
 }
 
+let input_count = 0;
 function speedTimer()
 {
-speed = document.getElementById("intext").value.length/sc*60;
+speed = input_count/sc*60;
 document.getElementById('speed_timer').value=speed.toFixed(2);
 sc=sc+3;
 t=setTimeout("speedTimer()",3000);
@@ -20,9 +24,16 @@ t=setTimeout("speedTimer()",3000);
 
 function timedCount()
 {
+    if(!is_paused){
     if(c>=0){
+        if(c<10) {
+            document.getElementById('timer').style.color = 'red';
+            const fontsize = getComputedStyle(document.getElementById('timer')).fontSize;
+            document.getElementById('timer').style.fontSize = (parseInt(fontsize)+1) + 'px';
+        }
 document.getElementById('timer').textContent=c;
 c--;
+document.getElementById('lifes').textContent = lifes;
 setTimeout("timedCount()",1000);}
 else{
     if(confirm("Начать заново?")){
@@ -30,19 +41,49 @@ else{
     }
 }
 }
+}
 
+function start_game() {
+    generate_text();
+    current_length = 0;
+    lifes = 3; 
+    document.getElementById('timer_block').style.visibility = 'visible';
+    document.getElementById('pause_Button').style.visibility = 'visible';
+    document.getElementById('intext').value = '';
+    document.getElementById('intext').disabled = false; 
+    // c = document.getElementById('task').textContent.length; 
+    с = 12;
+input_count=0;
+    timedCount(); 
+    speedTimer();
+}
+function pause_game() {
+is_paused=true;
+}
 
+function end_game() {
+
+}
+
+let current_length;
 function check(){
+    if (document.getElementById("intext").value.length > current_length) { 
+    input_count++;
+    }
+    current_length = document.getElementById("intext").value.length;
     if (document.getElementById("intext").value == document.getElementById("task").textContent)
-{document.getElementById("intext").style.borderColor="green";} 
+{document.getElementById("intext").style.borderColor="green";
+end_game();} 
 else{
     if (document.getElementById("intext").value == document.getElementById("task").textContent.substr(0,document.getElementById("intext").value.length))
     {document.getElementById("intext").style.borderColor="gray";
 }
     else{
         document.getElementById("intext").style.borderColor="red";
+        lifes--;
     }}
 };
+
 
 function generate_text() {
     var texts = new Array(15, "Ежедневно делайте зарядку и различные физические упражнения. Развивайте своё тело и дух.",
