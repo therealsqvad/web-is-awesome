cf29cc090b22e11cd46157f8fe56f6411e9316c2
therealsqvad <nikita_klepov@mail.ru>
2018-10-12 13:48:26 +0700

    сделано всё, кроме правильного сохранения рекордов и необходимы фиксы функционала

--- src/typeracer/typeracer.js
+++ src/typeracer/typeracer.js
@@ -1,105 +1,166 @@
+
 document.onselectstart = test;
-function test() {return false;}
-let c = 12;
-let sc =0;
-let speed = 0;
+function test() { return false; }
+
+let c;
+let t;
+let sc;
+let speed;
 let lifes;
-let is_paused = false;
-function new_game(){
-    console.log(getComputedStyle(document.getElementById('timer')).fontSize); 
-    setTimeout("document.getElementById('task').textContent='3...'",1000);
-    setTimeout("document.getElementById('task').textContent='2...'",2000);
-    setTimeout("document.getElementById('task').textContent='1...'",3000);
-    setTimeout(start_game,4000);     
+let is_paused = true;
+function new_game() {
+    document.getElementById('alreadytext').textContent = '';
+    setTimeout("document.getElementById('task').textContent='3...'", 1000);
+    setTimeout("document.getElementById('task').textContent='2...'", 2000);
+    setTimeout("document.getElementById('task').textContent='1...'", 3000);
+    setTimeout(start_game, 4000);
 }
 
 let input_count = 0;
-function speedTimer()
-{
-speed = input_count/sc*60;
-document.getElementById('speed_timer').value=speed.toFixed(2);
-sc=sc+3;
-t=setTimeout("speedTimer()",3000);
-}
-
-function timedCount()
-{
-    if(!is_paused){
-    if(c>=0){
-        if(c<10) {
-            document.getElementById('timer').style.color = 'red';
-            const fontsize = getComputedStyle(document.getElementById('timer')).fontSize;
-            document.getElementById('timer').style.fontSize = (parseInt(fontsize)+1) + 'px';
-        }
-document.getElementById('timer').textContent=c;
-c--;
-document.getElementById('lifes').textContent = lifes;
-setTimeout("timedCount()",1000);}
-else{
-    if(confirm("Начать заново?")){
-        new_game();
+function speedTimer() {
+    if (!is_paused) {
+        speed = input_count / sc * 60;
+        document.getElementById('speed_timer').value = speed.toFixed(2);
+        sc = sc + 3;
+        st = setTimeout("speedTimer()", 3000);
     }
 }
+
+function timedCount() {
+    if (!is_paused) {
+        if (c >= 0) {
+            if (c < 10) {
+                document.getElementById('timer').style.color = 'red';
+                const fontsize = getComputedStyle(document.getElementById('timer')).fontSize;
+                document.getElementById('timer').style.fontSize = (parseInt(fontsize) + 1) + 'px';
+            }
+            document.getElementById('timer').textContent = c;
+            c--;
+            t = setTimeout(timedCount, 1000);
+        }
+        else {
+            if (confirm("Начать заново?")) {
+                new_game();
+            }
+        }
+    }
 }
+
+function startTimer() {
+    if (is_paused) {
+        is_paused = false;
+        timedCount();
+        speedTimer();
+    }
+}
+
+function stopTimer() {
+    clearTimeout(t);
+    is_paused = true;
 }
 
 function start_game() {
+    document.getElementById('records').innerText=document.cookie;
+
     generate_text();
     current_length = 0;
-    lifes = 3; 
+    lifes = 3;
+    for (let i = 0; i < lifes; i++) {
+        if(!document.getElementById('life'+(i+1))){
+        let div = document.createElement('div');
+        div.className = "life_icon";
+        div.id = 'life' + (i + 1);
+        document.getElementById('lb').appendChild(div);}
+    }
     document.getElementById('timer_block').style.visibility = 'visible';
     document.getElementById('pause_Button').style.visibility = 'visible';
     document.getElementById('intext').value = '';
-    document.getElementById('intext').disabled = false; 
-    // c = document.getElementById('task').textContent.length; 
-    с = 12;
-input_count=0;
-    timedCount(); 
+    document.getElementById('intext').disabled = false;
+    c = document.getElementById('task').textContent.length;
+    input_count = 0;
+    sc = 0; speed = 0;
+    startTimer();
     speedTimer();
 }
+
 function pause_game() {
-is_paused=true;
+
+    if (!is_paused) {
+        stopTimer();
+        document.getElementById('pause_Button').textContent = 'Продолжить';
+        document.getElementById('intext').disabled = true; 
+    } else {
+        document.getElementById('pause_Button').textContent = 'Пауза';
+        document.getElementById('intext').disabled = false; 
+        startTimer();
+
+    }
+
+
 }
 
 function end_game() {
-
+    stopTimer();
+    document.cookie += speed.toFixed(2);
+    alert('Поздравляем!');
 }
 
 let current_length;
-function check(){
-    if (document.getElementById("intext").value.length > current_length) { 
-    input_count++;
+function check() {
+
+    if (document.getElementById("intext").value.length > current_length) {
+        input_count++;
     }
     current_length = document.getElementById("intext").value.length;
-    if (document.getElementById("intext").value == document.getElementById("task").textContent)
-{document.getElementById("intext").style.borderColor="green";
-end_game();} 
-else{
-    if (document.getElementById("intext").value == document.getElementById("task").textContent.substr(0,document.getElementById("intext").value.length))
-    {document.getElementById("intext").style.borderColor="gray";
-}
-    else{
-        document.getElementById("intext").style.borderColor="red";
-        lifes--;
-    }}
+    if (document.getElementById("intext").value === text) {
+        document.getElementById('alreadytext').textContent = document.getElementById("intext").value;
+        document.getElementById('task').textContent = '';
+        document.getElementById("intext").style.borderColor = "green";
+        end_game();
+    }
+    else {
+        if (document.getElementById("intext").value[document.getElementById("intext").value.length-1] === text[document.getElementById("intext").value.length-1]) {
+            document.getElementById("intext").style.borderColor = "gray";
+            document.getElementById('alreadytext').textContent = document.getElementById("intext").value;
+            document.getElementById('task').textContent = text.substr(document.getElementById('alreadytext').textContent.length, text.length-document.getElementById('alreadytext').textContent.length)
+        }
+        else {
+            document.getElementById("intext").style.borderColor = "red";
+
+            if (lifes > 0) {
+                document.getElementById('lb').removeChild(document.getElementById('life' + lifes))
+            }
+            else {
+                document.getElementById('intext').disabled = true;
+                stopTimer();
+                if (confirm("Вы проиграли:( Начать заново?")) {
+                    new_game();
+                }
+            }
+            lifes--;
+        }
+    }
 };
 
-
+let text;
 function generate_text() {
     var texts = new Array(15, "Ежедневно делайте зарядку и различные физические упражнения. Развивайте своё тело и дух.",
-            "Будьте благодарны Вселенной за каждый прожитый день.",
-            "Любите себя таким, какой вы есть, со всеми достоинствами и недостатками",
-            "Верьте только в самое лучшее и тогда в вашей жизни станет ещё больше приятных сюрпризов и хорошего настроения",
-            "Прочтите как можно больше биографий великих людей",
-            "Планируйте предстоящий день. Выделяйте наиболее приоритетные задачи, которые необходимо выполнить в первую очередь",
-            "Определите, что сейчас в жизни для вас наиболее важно и развивайтесь в этом направлении",
-            "Откажитесь от употребления алкоголя", "Пейте чистую воду и натуральные соки. Ограничьте употребление газированной воды",
-            "Пейте чистую воду и натуральные соки. Ограничьте употребление газированной воды", "Каждый день отмечайте для себя, насколько далеко вы продвинулись к своей цели. Помните, что каждый новый день - это шаг вперёд",
-            "Найдите для себя наставника. Учитесь у того, кто уже находится там, куда вы только стремитесь",
-            "Просыпайтесь в 6 утра. Утром можно успеть сделать гораздо больше дел, чем в течение дня, особенно, если вы творческий человек",
-            "Забудьте о своих недостатках. Вместо этого сосредоточьтесь на своих достоинствах",
-            "Ежемесячно откладывайте от 10 % от своего дохода. Инвестируйте в своё будущее",
-            "Контролируйте свои эмоции. Старайтесь как можно чаще улыбаться и быть в отличном настроении");
+        "Будьте благодарны Вселенной за каждый прожитый день.",
+        "Любите себя таким, какой вы есть, со всеми достоинствами и недостатками",
+        "Верьте только в самое лучшее и тогда в вашей жизни станет ещё больше приятных сюрпризов и хорошего настроения",
+        "Прочтите как можно больше биографий великих людей",
+        "Планируйте предстоящий день. Выделяйте наиболее приоритетные задачи, которые необходимо выполнить в первую очередь",
+        "Определите, что сейчас в жизни для вас наиболее важно и развивайтесь в этом направлении",
+        "Откажитесь от употребления алкоголя", "Пейте чистую воду и натуральные соки. Ограничьте употребление газированной воды",
+        "Пейте чистую воду и натуральные соки. Ограничьте употребление газированной воды", "Каждый день отмечайте для себя, насколько далеко вы продвинулись к своей цели. Помните, что каждый новый день - это шаг вперёд",
+        "Найдите для себя наставника. Учитесь у того, кто уже находится там, куда вы только стремитесь",
+        "Просыпайтесь в 6 утра. Утром можно успеть сделать гораздо больше дел, чем в течение дня, особенно, если вы творческий человек",
+        "Забудьте о своих недостатках. Вместо этого сосредоточьтесь на своих достоинствах",
+        "Ежемесячно откладывайте от 10 % от своего дохода. Инвестируйте в своё будущее",
+        "Контролируйте свои эмоции. Старайтесь как можно чаще улыбаться и быть в отличном настроении");
     var RandomNumber = Math.round(Math.random() * (15) + 1);
-    document.getElementById("task").textContent = texts[RandomNumber];
+    text=texts[RandomNumber];
+    document.getElementById("task").textContent = text;
+
+
 }
