035d3fd4d2533ecd151ea5dae1c40990a6e5d242
therealsqvad <nikita_klepov@mail.ru>
2018-10-18 00:46:09 +0700

    gitignore and eslint

--- src/typeracer/typeracer.js
+++ src/typeracer/typeracer.js
@@ -1,149 +1,159 @@
- import generate_text from './generate.js';
+import generateText from './generate';
 
-document.getElementById('new_game_Button').onclick = () => {new_game()};
-document.getElementById('pause_Button').onclick = () => {pause_game()};
-document.getElementById('intext').oninput = () => {check()};
-document.onselectstart = test;
 function test() { return false; }
+document.onselectstart = test;
 
 let text;
 let c;
 let t;
 let sc;
+let st;
 let speed;
 let lifes;
-let is_paused = true;
-function new_game() {
-    document.getElementById('alreadytext').textContent = '';
-    setTimeout("document.getElementById('task').textContent='3...'", 1000);
-    setTimeout("document.getElementById('task').textContent='2...'", 2000);
-    setTimeout("document.getElementById('task').textContent='1...'", 3000);
-    setTimeout(start_game, 4000);
+let inputCount = 0;
+let isPaused = true;
+
+function newGame() {
+  document.getElementById('alreadytext').textContent = '';
+  setTimeout(settime('3...'), 1000);
+  setTimeout(settime('2...'), 2000);
+  setTimeout(settime('1...'), 3000);
+  setTimeout(startGame, 4000);
 }
 
-let input_count = 0;
 function speedTimer() {
-    if (!is_paused) {
-        speed = input_count / sc * 60;
-        document.getElementById('speed_timer').value = speed.toFixed(2);
-        sc = sc + 3;
-        let st = setTimeout(speedTimer, 3000);
-    }
+  if (!isPaused) {
+    speed = inputCount / sc * 60;
+    document.getElementById('speed_timer').value = speed.toFixed(2);
+    sc += 3;
+    st = setTimeout(speedTimer, 3000);
+  }
 }
 
 function timedCount() {
-    if (!is_paused) {
-        if (c >= 0) {
-            if (c < 10) {
-                document.getElementById('timer').style.color = 'red';
-                const fontsize = getComputedStyle(document.getElementById('timer')).fontSize;
-                document.getElementById('timer').style.fontSize = (parseInt(fontsize) + 1) + 'px';
-            }
-            document.getElementById('timer').textContent = c;
-            c--;
-            t = setTimeout(timedCount, 1000);
-        }
-        else {
-            if (confirm("Начать заново?")) {
-                new_game();
-            }
-        }
+  if (!isPaused) {
+    if (c >= 0) {
+      if (c < 10) {
+        document.getElementById('timer').style.color = 'red';
+        const fontsize = getComputedStyle(document.getElementById('timer')).fontSize;
+
+        document.getElementById('timer').style.fontSize = `${parseInt(fontsize, 2) + 1}px`;
+      }
+      document.getElementById('timer').textContent = c;
+      c--;
+      t = setTimeout(timedCount, 1000);
+    } else if (confirm('Начать заново?')) {
+      newGame();
     }
+  }
 }
 
 function startTimer() {
-    if (is_paused) {
-        is_paused = false;
-        timedCount();
-        speedTimer();
-    }
+  if (isPaused) {
+    isPaused = false;
+    timedCount();
+    speedTimer();
+  }
 }
+
+function startGame() {
+  text = generateText();
+  currentLength = 0;
+  lifes = 3;
+  inputCount = 0;
+  sc = 0; speed = 0;
+  initializeHTML();
+  startTimer();
+  speedTimer();
+}
+
+function settime(txt) {
+  document.getElementById('task').textContent = txt;
+}
+
 
 function stopTimer() {
-    clearTimeout(t);
-    is_paused = true;
+  clearTimeout(t);
+  clearTimeout(st);
+  isPaused = true;
 }
 
-function start_game() {
-    document.getElementById('records').innerText=document.cookie;
+function initializeHTML() {
+  document.getElementById('records').innerText = document.cookie;
+  for (let i = 0; i < lifes; i++) {
+    if (!document.getElementById(`life${i + 1}`)) {
+      const div = document.createElement('div');
 
-    text = generate_text();
-    current_length = 0;
-    lifes = 3;
-    for (let i = 0; i < lifes; i++) {
-        if(!document.getElementById('life'+(i+1))){
-        let div = document.createElement('div');
-        div.className = "life_icon";
-        div.id = 'life' + (i + 1);
-        document.getElementById('lb').appendChild(div);}
+      div.className = 'life_icon';
+      div.id = `life${i + 1}`;
+      document.getElementById('lb').appendChild(div);
     }
-    document.getElementById('timer_block').style.visibility = 'visible';
-    document.getElementById('pause_Button').style.visibility = 'visible';
-    document.getElementById('intext').value = '';
-    document.getElementById('intext').disabled = false;
-    c = document.getElementById('task').textContent.length;
-    input_count = 0;
-    sc = 0; speed = 0;
-    startTimer();
-    speedTimer();
+  }
+  document.getElementById('timer_block').style.visibility = 'visible';
+  document.getElementById('pause_Button').style.visibility = 'visible';
+  document.getElementById('intext').value = '';
+  document.getElementById('intext').disabled = false;
+  c = document.getElementById('task').textContent.length;
 }
 
-function pause_game() {
 
-    if (!is_paused) {
-        stopTimer();
-        document.getElementById('pause_Button').textContent = 'Продолжить';
-        document.getElementById('intext').disabled = true; 
-    } else {
-        document.getElementById('pause_Button').textContent = 'Пауза';
-        document.getElementById('intext').disabled = false; 
-        startTimer();
-
-    }
-
-
-}
-
-function end_game() {
+function pauseGame() {
+  if (!isPaused) {
     stopTimer();
-    document.cookie += speed.toFixed(2);
-    alert('Поздравляем!');
+    document.getElementById('pause_Button').textContent = 'Продолжить';
+    document.getElementById('intext').disabled = true;
+  } else {
+    document.getElementById('pause_Button').textContent = 'Пауза';
+    document.getElementById('intext').disabled = false;
+    startTimer();
+  }
 }
 
-let current_length;
+function endGame() {
+  stopTimer();
+  document.cookie += speed.toFixed(2);
+  alert('Поздравляем!');
+}
+
+let currentLength;
+
 function check() {
+  if (document.getElementById('intext').value.length > currentLength) {
+    inputCount++;
+  }
+  currentLength = document.getElementById('intext').value.length;
+  if (document.getElementById('intext').value === text) {
+    win();
+  } else if (document.getElementById('intext').value[document.getElementById('intext').value.length - 1] === text[document.getElementById('intext').value.length - 1]) {
+    document.getElementById('intext').style.borderColor = 'gray';
+    document.getElementById('alreadytext').textContent = document.getElementById('intext').value;
+    document.getElementById('task').textContent = text.substr(document.getElementById('alreadytext').textContent.length, text.length - document.getElementById('alreadytext').textContent.length);
+  } else {
+    error();
+  }
+}
 
-    if (document.getElementById("intext").value.length > current_length) {
-        input_count++;
-    }
-    current_length = document.getElementById("intext").value.length;
-    if (document.getElementById("intext").value === text) {
-        document.getElementById('alreadytext').textContent = document.getElementById("intext").value;
-        document.getElementById('task').textContent = '';
-        document.getElementById("intext").style.borderColor = "green";
-        end_game();
-    }
-    else {
-        if (document.getElementById("intext").value[document.getElementById("intext").value.length-1] === text[document.getElementById("intext").value.length-1]) {
-            document.getElementById("intext").style.borderColor = "gray";
-            document.getElementById('alreadytext').textContent = document.getElementById("intext").value;
-            document.getElementById('task').textContent = text.substr(document.getElementById('alreadytext').textContent.length, text.length-document.getElementById('alreadytext').textContent.length)
-        }
-        else {
-            document.getElementById("intext").style.borderColor = "red";
+function win() {
+  document.getElementById('alreadytext').textContent = document.getElementById('intext').value;
+  document.getElementById('task').textContent = '';
+  document.getElementById('intext').style.borderColor = 'green';
+  endGame();
+}
 
-            if (lifes > 0) {
-                document.getElementById('lb').removeChild(document.getElementById('life' + lifes))
-            }
-            else {
-                document.getElementById('intext').disabled = true;
-                stopTimer();
-                if (confirm("Вы проиграли:( Начать заново?")) {
-                    new_game();
-                }
-            }
-            lifes--;
-        }
+function error() {
+  document.getElementById('intext').style.borderColor = 'red';
+  if (lifes > 0) {
+    document.getElementById('lb').removeChild(document.getElementById(`life${lifes}`));
+  } else {
+    document.getElementById('intext').disabled = true;
+    stopTimer();
+    if (confirm('Вы проиграли:( Начать заново?')) {
+      newGame();
     }
-};
+  }
+  lifes--;
+}
 
+document.getElementById('new_game_Button').onclick = () => { newGame(); };
+document.getElementById('pause_Button').onclick = () => { pauseGame(); };
+document.getElementById('intext').oninput = () => { check(); };
