d062bd72a200876646c15f59e9927bbf2b21db63
therealsqvad <nikita_klepov@mail.ru>
2018-10-18 13:24:28 +0700

    all replaced

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,9 +1,27 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
   entry: './typeracer/typeracer.js',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname + '/typeracer', 'dist')
+    path: path.resolve(`${__dirname}/typeracer`, 'dist'),
+    sourceMapFilename: 'main.map'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devServer: {
+    contentBase: path.join(`${__dirname}`, ''),
+    port: 9000
   }
 };
